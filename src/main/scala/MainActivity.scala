b8a5227e18a803f42c534215b575fa1dae538a97
Robert Thau <rst@ai.mit.edu>
2012-10-05 17:05:28 -0400

    Add an itsy-bitsy, teeny-weeny test of the reflection API itself.

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -3,11 +3,20 @@ package rst.reflection_test
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
 
+import scala.reflect.runtime.{universe => ru}
+import ru._
+
 class MainActivity extends Activity with TypedActivity {
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
 
-    findView(TR.textview).setText("hello, world!")
+    findView(TR.textview).setText("reflection not run yet")
+
+    val TypeRef(optType, optClass, dummyArgs) = typeOf[Option[Any]]
+    typeOf[Option[String]] match {
+      case TypeRef(optType, optClass, List(arg)) => 
+        findView(TR.textview).setText("got " + arg)
+    }
   }
 }
